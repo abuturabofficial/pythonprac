0d0806476681d5865a7ecafc82db30e368398b82
AbuTurab <68259771+abuturabofficial@users.noreply.github.com>
2026-09-22 07:04:20 +0500

    fixt formatting week 4: libs

--- cs50p/week4-libraries/itunes.py
+++ cs50p/week4-libraries/itunes.py
@@ -5,9 +5,11 @@ import sys
 if len(sys.argv) != 2:
     sys.exit("Not enough arguments.")
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
+response = requests.get(
+    "https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
 
 o = response.json()
+print(o)
 
 for result in o["results"]:
     print(result["trackName"])
